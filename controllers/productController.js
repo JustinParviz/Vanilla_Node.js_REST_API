e0fcc1b8fa8df05355f900f64b7bf4fad9a5f6a7
JustinParviz <jarshravan@gmail.com>
2025-02-11 17:32:47 -0500

    Created the Model and Controller

---
+++ controllers/productController.js
@@ -0,0 +1,3 @@
+// Controllers are basically going to control whatever that particular route is doing, the status its sending, what
+// it's sending back, any headers we want to send and it's also going to interact with the model since it'll get the 
+// data from the model to send back.
---
+++ models/productModel.js
@@ -0,0 +1,2 @@
+// Models deal with data (in this case we're dealing with a JSON file), so these functions will strictly be used 
+// to either Get data or Create or Update or Delete data.
