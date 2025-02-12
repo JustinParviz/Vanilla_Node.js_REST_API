4c3f6970bf64d47c63368eadf12ea48ebd5894d5
JustinParviz <jarshravan@gmail.com>
2025-02-11 23:28:47 -0500

    Structured my API to be able to GET all products using the Server, Controller, and Model components that I created

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,21 @@
 // Controllers are basically going to control whatever that particular route is doing, the status its sending, what
 // it's sending back, any headers we want to send and it's also going to interact with the model since it'll get the 
 // data from the model to send back.
+
+const Product = require("../models/productModel")
+
+async function getProducts(req, res) {
+    try {
+        const products = await Product.findAll()
+
+        res.writeHead(200, { "Content-Type": "application/json" })
+        res.end(JSON.stringify(products))
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+module.exports = {
+    getProducts
+}
+
--- models/productModel.js
+++ models/productModel.js
@@ -1,2 +1,17 @@
 // Models deal with data (in this case we're dealing with a JSON file), so these functions will strictly be used 
 // to either Get data or Create or Update or Delete data.
+
+const products = require("../data/products")
+
+function findAll() {
+    return new Promise((resolve, reject) => {
+        resolve(products)
+    })
+}
+
+module.exports = {
+    findAll
+}
+
+
+
