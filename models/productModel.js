651aa939af38bdf1f95bbd58e2c96b4e6b4f7ab6
JustinParviz <jarshravan@gmail.com>
2025-02-16 15:49:49 -0500

    Created function to find each product by ID

--- models/productModel.js
+++ models/productModel.js
@@ -9,8 +9,16 @@ function findAll() {
     })
 }
 
+function findById(id) {
+    return new Promise((resolve, reject) => {
+        const product = products.find((p) => p.id === id)
+        resolve(product)
+    })
+}
+
 module.exports = {
-    findAll
+    findAll,
+    findById
 }
 
 
